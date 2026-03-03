3e93afee6a3dce3fd8e28234f72cfcd3be15e5f7
guzalturaeva025-ai <guzalturaeva025@gmail.com>
2026-03-04 04:41:53 +0500

    app.py

--- app.py
+++ app.py
@@ -43,13 +43,15 @@ Student Text:
 {text}
 """
 
-        response = client.chat.completions.create(
-            model="llama3-8b-8192",
-            messages=[{"role": "user", "content": prompt}],
-        )
+       response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[{"role": "user", "content": prompt}],
+    temperature=0.7,
+)
 
         feedback = response.choices[0].message.content
 
         st.subheader("Feedback Report")
         st.write(feedback)
+
 
