56cbfbf44332a03f523de42775df03f01dd48692
guzalturaeva025-ai <guzalturaeva025@gmail.com>
2026-03-15 13:53:43 +0500

    app.py

--- app.py
+++ app.py
@@ -86,18 +86,32 @@ Student Text:
 """
 
     # --- AI RESPONSE (FEEDBACK) ---
+    try:
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
-        messages=[{"role":"user","content":prompt}],
+        messages=[{"role": "user", "content": prompt}],
         temperature=0.7,
     )
 
     feedback = response.choices[0].message.content
 
+except Exception as e:
+    st.error("AI feedback generation failed.")
+    st.stop()
 
     # --- ERROR CORRECTION PROMPT ---
-    error_prompt = f"""
-You are an English teacher.
+  try:
+    error_response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[{"role":"user","content":error_prompt}],
+        temperature=0.2,
+    )
+
+    error_feedback = error_response.choices[0].message.content
+
+except Exception as e:
+    st.error("Error correction generation failed.")
+    st.stop()
 
 Analyze the student's text and identify grammar, vocabulary, and spelling mistakes.
 
